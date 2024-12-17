5e86f907f693df551bb09bfc680b8d8c30ed91d2
jaron-bauers <jaron.bauers@gmail.com>
2024-12-16 23:04:56 -0700

    v1 refactor

--- background.js
+++ background.js
@@ -6,40 +6,40 @@ chrome.action.onClicked.addListener((tab) => {
 });
 
 async function main() {
-    const adInfoIcon = "ytp-ad-button";
-    const buttons = document.getElementsByClassName(adInfoIcon);
-    
-    for (const button of buttons) {
-        console.log("Clicking Ad Info Button...");
-        button.click();
-    }
+    // Click "ad center"
+    const adCenterButton = document.getElementsByClassName('ytp-ad-button');
+    console.log('Clicking Ad Center Button...');
+    adCenterButton[0].click();
 
-    // Directly attempt to click buttons inside iframes
-    await sleep(1000);
-    clickIframeElements("mUIrbf-LgbsSe", "2");
-    await sleep(1000);
-    clickIframeElements("WvipBf-LgbsSe-LoDsGd", "3");
-    await sleep(1000);
-    clickIframeElements("VfPpkd-Bz112c-LgbsSe", "4");
+    // Click "Block ad"
+    await sleep(650);
+    clickIframeEements('.mUIrbf-LgbsSe', '2');
 
-    function clickIframeElements(buttonClass, test) {
-        const iframe = document.querySelector("iframe");
+    // Click "Continue"
+    await sleep(500);
+    clickIframeElements('.WvipBf-LgbsSe-LoDsGd', '3');
+
+    // Click exit X
+    clickIframeElements('.VfPpkd-Bz112c-LgbsSe', '4');
+
+    function clickIframeElements(buttonClass) {
+        const iframe = document.querySelector('iframe');
     
         if (!iframe) {
-            console.log("Iframe not found.");
-            return;
+            throw new Error('FAILED: iframe not found');
         }
     
-        const iframeDocument = iframe.contentDocument || iframe.contentWindow.document; // Access the iframe's document
-        const button = iframeDocument.querySelector(`.${buttonClass}`); // Find the button by class
+        // Access the iframe's document
+        const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
 
-        console.log(button);
+        // Find the button by class
+        const button = iframeDocument.querySelector(buttonClass); // Find the button by class
     
         if (button) {
-            console.log(`Clicking element inside iframe! ${test}`);
+            console.log(`Clicking ${buttonClass} Element...`);
             button.click();
         } else {
-            console.log(`Element not found inside iframe. ${test}`);
+            throw new Error('FAILED: Element not found');
         }
     }
 
